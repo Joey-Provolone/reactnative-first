7aabd960e6e230f2f16e1f10aefb9f02ac4d3ff4
aumdove <aumdove13@gmail.com>
2020-03-14 20:28:11 -0500

    added color counter functions to increment or decrement color calues by 15

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const ColorCounter = ({ color }) => {
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text>{color}</Text>
-      <Button title={`Increase ${color}`} />
-      <Button title={`Increase ${color}`} />
+      <Button
+        onPress={() => onIncrease()}
+        title={`Increase ${color}`}
+      />
+      <Button
+        onPress={() => onDecrease()}
+        title={`Increase ${color}`}
+      />
     </View>
   );
 };
